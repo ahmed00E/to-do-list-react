11eecc547c982666dd336e00b7af11af948bbcdc
ahmed00E <84589474+ahmed00E@users.noreply.github.com>
2021-11-02 17:24:01 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            userInput: '',
+            items: []     /*add*/
+        };
+    }
+
+    onChange(event) {
+        this.setState({
+            userInput: event.target.value
+        });
+    }
+
+    addTodo(event) {
+        event.preventDefault();
+        this.setState({
+            userInput: '',
+            items: [...this.state.items, this.state.userInput]
+        });
+    }
+
+    deleteTodo(item) {
+        // no event 
+        // pass the item to indexOf
+        const array = this.state.items;
+        const index = array.indexOf(item);
+        array.splice(index, 1);
+        this.setState({
+            items: array
+        });
+    }
+
+    // add item to deleteTodo.bind(this, item)
+    renderTodos() {
+        return this.state.items.map((item) => {
+            return (
+                <div className="list-group-item" key={item}>
+                    {item} | <button onClick={this.deleteTodo.bind(this, item)}>Supprimer</button>
+                </div>    
+            );
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                 
+                <form className="form-row align-items-center">
+                    <input 
+                        value={this.state.userInput} 
+                        type="text" 
+                        placeholder="Renseigner un item"
+                        onChange={this.onChange.bind(this)}
+                        className="form-control mb-2"
+                    />
+                    <button 
+                        onClick={this.addTodo.bind(this)} 
+                        className="btn btn-primary"
+                    >
+                        Ajouter a ma liste
+                    </button>
+                </form>
+                <div className="list-group">
+                    {this.renderTodos()}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
